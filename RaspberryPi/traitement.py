08d7a8cb6f9ed340a897807086e9d81380f2cd6f
Behrouz <nikb2701@usherbrooke.ca>
2024-04-06 22:28:41 -0400

    pres

--- RaspberryPi/traitement.py
+++ RaspberryPi/traitement.py
@@ -1,37 +1,42 @@
-recent_data = []  # This will hold the most recent data points for activity detection
-post_activity_data = []  # To collect data after detecting activity
-activity_detected = False
+import numpy as np
 
-def detect_and_format_activity(emg_value, threshold=10, total_length=100):
-    global recent_data, post_activity_data, activity_detected
-    formatted_data = None
 
-    # Add the new value to the recent data
-    recent_data.append(emg_value)
-    print(f"new traitement value: {emg_value}")
-    # Check for muscle activity detection logic
-    if not activity_detected and len(recent_data) == 10:
-        # Calculate the mean of the last 10 values
-        mean_of_last_10 = sum(recent_data[0:-1]) / 10
+
+class Preprocess:
+
+    def __init__(self, threshold, dataQuantity = 40):
+        self.threshold = threshold
+        self.dataQuantity = dataQuantity
+        self.activity_detected = False
+        self.formatted_data = None
+        self.recent_data = []
+        self.post_activity_data = []
+
+    def detect_format_activity(self, data):
+        if data == 49:
+            pass
+        if not self.activity_detected:
+            self.recent_data.append(data)
+        if not self.activity_detected and len(self.recent_data)>=2:
+            # Calculate the mean of the last 10 values
+            mean_of_last_2 = np.mean(self.recent_data[-2:-1])
+            print(mean_of_last_2)
         
-        if abs(emg_value - mean_of_last_10) > threshold:
-            activity_detected = True
-            print(f"spike detected at value: {emg_value}")
-            # Include the triggering value as the first of the post-activity values
-            post_activity_data.append(emg_value)
+            if abs(data - mean_of_last_2) > self.threshold:
+                self.activity_detected = True
+                print(f"spike detected at value: {data}")
+                # Include the triggering value as the first of the post-activity values
+                self.post_activity_data.append(data)
 
-    elif activity_detected:
-        post_activity_data.append(emg_value)
-        # Once we have collected 20 values after the activity detection, format the data
-        if len(post_activity_data) == 20:
-            formatted_data = [0] * 40 + post_activity_data + [0] * 40
-            # Reset for next detection
-            recent_data = []
-            post_activity_data = []
-            activity_detected = False
+        elif self.activity_detected:
 
-    # Ensure we only keep the most recent data points for future analysis
-    if len(recent_data) > 20:
-        recent_data.pop(0)
-
-    return formatted_data
+            self.post_activity_data.append(data)
+            # Once we have collected 40 values after the activity detection, format the data
+            if len(self.post_activity_data) == 40:
+                self.formatted_data = [0] * 30 + self.post_activity_data + [0] * 30
+                # Reset for next detection
+                self.recent_data = []
+                self.post_activity_data = []
+                self.activity_detected = False
+        
+        
